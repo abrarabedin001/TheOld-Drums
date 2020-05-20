dec8fad8751590a8d142f842730d01b910a7254f
abrarabedin001 <abrarabedin001@gmail.com>
2020-05-20 17:17:56 +0600

    Updates

---
+++ precache-manifest.f1a898a3c0a5a0340c7c6e297c73dd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4f07fea6e6192673df65e94d6c2ccb",
+    "url": "/TheOld-Drums/index.html"
+  },
+  {
+    "revision": "5d4994ab7c62e5c5eff1",
+    "url": "/TheOld-Drums/static/css/main.fac562e6.chunk.css"
+  },
+  {
+    "revision": "1694799ebc931b34cf58",
+    "url": "/TheOld-Drums/static/js/2.3b53d1cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheOld-Drums/static/js/2.3b53d1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4994ab7c62e5c5eff1",
+    "url": "/TheOld-Drums/static/js/main.a29e0388.chunk.js"
+  },
+  {
+    "revision": "ade56aaa7992f947a0c5",
+    "url": "/TheOld-Drums/static/js/runtime-main.ea2ca30a.js"
+  }
+]);
