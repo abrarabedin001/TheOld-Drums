f83d8635eb974cf4f876198ab4f936f752b6b2b2
abrarabedin001 <abrarabedin001@gmail.com>
2020-05-20 16:46:15 +0600

    Updates

---
+++ precache-manifest.c10ea350f15436eca067de779a76636c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c9d05211ba9e641d1d7c4131f58ba1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ca18fab77cb70346a9ee",
+    "url": "/react-gh-pages/static/css/main.fac562e6.chunk.css"
+  },
+  {
+    "revision": "1694799ebc931b34cf58",
+    "url": "/react-gh-pages/static/js/2.3b53d1cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3b53d1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca18fab77cb70346a9ee",
+    "url": "/react-gh-pages/static/js/main.85d6ea21.chunk.js"
+  },
+  {
+    "revision": "6ff939d621f4b1c08e3f",
+    "url": "/react-gh-pages/static/js/runtime-main.39256a51.js"
+  }
+]);
